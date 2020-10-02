e7593034dc93c172d0e79054f98cf92f610c4706
olyapetryshyn <olya.petryshyn@gmail.com>
2020-10-02 15:31:46 +0300

    added a task to wait for the external task to finish.

--- dags/dags1/trigger_dag.py
+++ dags/dags1/trigger_dag.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
 
 default_args = {
     'owner': 'Olya',
@@ -21,6 +22,8 @@ with DAG('gridu_trigger_dag', default_args=default_args, schedule_interval='@hou
     check_for_a_file = FileSensor(task_id='check_for_a_file', filepath=path_to_run_file)
     trigger_dag = TriggerDagRunOperator(task_id='trigger_dag', trigger_dag_id='gridu_dag',
                                         python_callable=lambda context, dag_run_obj: dag_run_obj)
+    wait_to_finish_dag = ExternalTaskSensor(task_id='wait_for_dag', external_dag_id='gridu_dag',
+                                            external_task_id='query_the_table', allowed_states=['success', 'skipped'])
     remove_a_file = BashOperator(task_id='remove_a_file', bash_command='rm {}'.format(path_to_run_file))
 
-    check_for_a_file >> trigger_dag >> remove_a_file
+    check_for_a_file >> trigger_dag >> wait_to_finish_dag >> remove_a_file
